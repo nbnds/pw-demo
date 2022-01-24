921133a614448a914ca22558fed37bf271a31102
Alexej Simakov <127182+nbnds@users.noreply.github.com>
2022-01-24 07:17:07 +0000

    added test content with screenhot and assertion

---
+++ sample-tests/tests/test_example.py
@@ -0,0 +1,4 @@
+def test_open_playwright_site(page):
+    page.goto("https://playwright.dev")
+    page.screenshot(path="playwright_dev.png")
+    assert "Fast and realiable end-to-end" in page.title()
